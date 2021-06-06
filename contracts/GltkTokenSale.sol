d857f3766e133cb658ef0f1c84dc3d5a27043dd2
Otavioensa <otavioensa@gmail.com>
2021-06-06 21:38:53 +0200

    Add token price

--- contracts/GltkTokenSale.sol
+++ contracts/GltkTokenSale.sol
@@ -5,12 +5,14 @@ import "./GalacticToken.sol";
 contract GltkTokenSale {
   address admin;
   GalacticToken public tokenContract;
+  uint256 public tokenPrice;
+
 
   constructor(GalacticToken _tokenContract) public {
     // whoever deploys the contract is the admin
     // token contract
     admin = msg.sender;
     tokenContract = _tokenContract;
-    // token price
+    tokenPrice = 1000000000000000;
   }
 }
--- test/GltkTokenSale.js
+++ test/GltkTokenSale.js
@@ -1,14 +1,19 @@
 const GltkTokenSale = artifacts.require("GltkTokenSale");
 
-contract.only('GltkTokenSale', (accounts) => {
+contract('GltkTokenSale', (accounts) => {
   let gltkTokenSale
   it('initializes the contract with correct values', async () => {
     gltkTokenSale = await GltkTokenSale.deployed()
 
+    // 0.001 ether
+    const expectedTokenPrice = 1000000000000000
+
     const address = gltkTokenSale.address
     const tokenContract = await gltkTokenSale.tokenContract()
+    const tokenPrice = await gltkTokenSale.tokenPrice()
 
     assert.notEqual(address, '', 'has contract address')
     assert.notEqual(tokenContract, '', 'has token contract')
+    assert.equal(tokenPrice, expectedTokenPrice)
   })
 })
